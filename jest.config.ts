9a9e335a9e3187d7e4bd671da2235bfcaee80758
Raul <you@example.com>
2023-02-20 17:50:32 +0100

    starter code

--- jest.config.ts
+++ jest.config.ts
@@ -7,6 +7,8 @@ export default {
         '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__mocks__/fileMock.js',
         '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
       },
+      setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+
     }
   /*
   Un objeto de identidad que usa proxies ES6. Útil para probar importaciones triviales de paquetes web. Por ejemplo, puede decirle a Jest que simule este objeto como módulos CSS importados; entonces todas sus búsquedas de nombre de clase en el objeto de estilos importados se devolverán tal cual. */
